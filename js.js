0418f616cb4cb223578b9072c715f932862913f4
Abhishek <abh7shekcr64@gmail.com>
2025-06-03 11:58:10 +0530

    learnt premitive datatypes

--- js.js
+++ js.js
@@ -1,14 +1,21 @@
-let value = "this is the\" first line\"  \n \\ this is the\' second \t line";
-// console.log(value);
+/* let value = "this is the\" first line\"  \n \\ this is the\' second \t line";
+ console.log(value);
 let half = `half of 100 is ${100/2}` //if we use backticks at the place of dual quotes we can perform mathematical operation within same line.
-//console.log(half);
-//console.log(typeof half);
-//console.log(-(10-2));
-//console.log(4>5);
+console.log(half);
+console.log(typeof half);
+console.log(-(10-2));
+console.log(4>5);
 //comarison operator
 console.log("apple" =="orange");
 console.log("Apple"!= "Samsung");
 console.log(NaN > NaN);
 console.log("Pizza" > "Burger");             
     console.log("hellowrot");
-    console.log("hi anothr commit") ;                                                                                                                                                      
+    console.log("hi anothr commit");                                                                
+    */     
+   //Datatypes                                                                   
+ let value = 43;
+ let negativeValue = -(value);
+ console.log(negativeValue);
+let bigInteger = 8765765777985798537n;  // n is used at the end of the value to define BIG INT datatype 
+console.log( typeof bigInteger);
